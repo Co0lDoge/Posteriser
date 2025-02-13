808043c77ebecac65dfab37a353de731fea9a84b
CoolDoge <thedarkguard60@gmail.com>
2025-02-13 22:28:58 +0500

    Update .gitignore and modify model path in TextCorrector to use model_path

--- text_transform.py
+++ text_transform.py
@@ -5,7 +5,8 @@ class TextCorrector:
         self.corrector = corrector
 
     def get_default_corrector():
-        return TextCorrector(corrector=pipeline("text2text-generation", model="ai-forever/sage-fredt5-large"))
+        model_path = "model\sage-fredt5-large"
+        return TextCorrector(corrector=pipeline("text2text-generation", model=model_path))
     
     def fix_spelling(self, text) -> str:
         return self.corrector(text)[0]['generated_text']
