92b1ed15285945d1bc05e808d628fe7188f523ab
Ivan <thedarkguard60@gmail.com>
2025-02-24 12:57:40 +0500

    update templane_noman description

--- template/templates/template_noman.py
+++ template/templates/template_noman.py
@@ -24,17 +24,17 @@ def get_template_noman() -> Template:
             text_alignment=TextAlignment.LEFT
         ),
         event_title=DrawableText(
-            size=(1400, 250),
+            size=(1400, 400),
             position=(int(background_size/2-1400/2), 200),
             font_path=DEFAULT_FONT_BOLD,
-            font_size=80,
+            font_size=100,
             font_color=DEFAULT_COLOR,
             text_alignment=TextAlignment.CENTER,
             text_line=TextLine.VERTICAL.set_line_width(DEFAULT_LINE_WIDTH)
         ),
         event_description=DrawableText(
             size=(1000, 800),
-            position=(220, 440),
+            position=(220, 500),
             font_path=DEFAULT_FONT,
             font_size=70,
             font_color=DEFAULT_COLOR,
